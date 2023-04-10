5b6275a897f17c9123b66087c5b47b9ac67e4012
Lee-Weaver <leeweaver23@gmail.com>
2023-04-10 18:16:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,42 @@ let playButton = document.querySelector(".play-button");
 let currentScore = document.querySelector(".current-score");
 let highScore = document.querySelector(".high-score");
 
+
 let level = 0;
-const answer = [1,2,3,4];
+const answerArr = [1,2,3,4];
+const playerArr = [];
 playAnswer();
 
+
+yellow.addEventListener("click", () => pushToArr(playerArr, 1));
+blue.addEventListener("click", () => pushToArr(playerArr, 2));
+red.addEventListener("click", () => pushToArr(playerArr, 4));
+green.addEventListener("click", () => pushToArr(playerArr, 3));
+playButton.addEventListener("click", () => {
+    playButton.innerHTML = "Started";
+    gameLoop();
+})
+
+function gameLoop() {
+
+}
+
+function pushToArr(arr, num) {
+    arr.push(num);
+    console.log(arr);
+}
+function playersGuess() {
+    let arr = [];
+    yellow.addEventListener("click", () => arr.push(1));
+    blue.addEventListener("click", () => arr.push(2));
+    red.addEventListener("click", () => arr.push(3));
+    green.addEventListener("click", () => arr.push(4));
+
+    if (arr.length > 5) {
+        console.log(playerArr);
+        return playerArr = arr;
+    }
+}
 function lightTile(button) {
     button.classList.add("light");
     setTimeout(()=> {
@@ -17,7 +49,7 @@ function lightTile(button) {
     }, 300);
 }
 function playAnswer() {
-    answer.forEach((num, index) => { setTimeout(()=>{
+    answerArr.forEach((num, index) => { setTimeout(()=>{
         switch(num) {
             case 1:
                 lightTile(yellow);
